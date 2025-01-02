5efddda287ef65fa799ba3dc1d87e2dc4e57fc2a
D4rkMn <d4rkmoon0503@gmail.com>
2025-01-02 04:16:15 -0500

    Started rework for gpt-4o-mini

--- bot/Cogs/Chat/GptConnector.py
+++ bot/Cogs/Chat/GptConnector.py
@@ -1,4 +1,4 @@
-import openai
+from openai import OpenAI
 
 # import the base interface for the llm connector
 from bot.Cogs.Chat.iLlmConnector import iLlmConnector
@@ -10,14 +10,27 @@ from bot.Cogs.Chat.iLlmConnector import iLlmConnector
 #
 class GptConnector(iLlmConnector):
     def __init__(self, _api_key : str, _maxTokens : int):
-        openai.api_key = _api_key
+        self.client = OpenAI(api_key = _api_key)
         self.maxTokens = _maxTokens
 
     def generateResponse(self, inputText : str) -> str:
-        response = openai.completions.create(
-            model = "gpt-3.5-turbo-instruct",
-            prompt = inputText,
-            max_tokens= self.maxTokens
+        response = self.client.chat.completions.create(
+            model = "gpt-4o-mini",
+            messages = [{
+                "role": "user",
+                "content": [{
+                    "type": "text",
+                    "text": inputText
+                }]
+            }],
+            response_format = {
+                "type": "text"
+            },
+            temperature = 1.5,
+            max_completion_tokens = self.maxTokens,
+            top_p = 1,
+            frequency_penalty = 0,
+            presence_penalty = 0
         )
-        result = response.choices[0].text
+        result = response.choices[0].message.content
         return result
